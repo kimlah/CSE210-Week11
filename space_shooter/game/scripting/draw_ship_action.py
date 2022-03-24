83dc5070a5d3114a569318fa3e173b6c8f2815af
Kathryn Thompson <katyounge@hotmail.com>
2022-03-24 10:10:08 -0700

    added draw_ship_action class and changed name of move_ship_action class

---
+++ space_shooter/game/scripting/draw_ship_action.py
@@ -0,0 +1,21 @@
+from constants import *
+from game.scripting.action import Action
+
+
+class DrawShipAction(Action):
+
+    def __init__(self, video_service):
+        self._video_service = video_service
+        
+    def execute(self, cast, script, callback):
+        ship = cast.get_first_actor(SHIP_GROUP)
+        body = ship.get_body()
+
+        if ship.is_debug():
+            rectangle = body.get_rectangle()
+            self._video_service.draw_rectangle(rectangle, PURPLE)
+            
+        
+        image = SHIP_IMAGE
+        position = body.get_position()
+        self._video_service.draw_image(image, position)
